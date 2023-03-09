9db645fcdf988e4aa8be5c10cca86eb680d4e953
NiklasSillaber <91011800+NiklasSillaber@users.noreply.github.com>
2023-03-09 15:34:18 +0100

    Up

---
+++ HCIN_SWP_Alexa_VVT/Models/VVT_API.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+
+namespace HCIN_SWP_Alexa_VVT.Models
+{
+    public class VVT_API
+    {
+        List<VVT_Station> Stations;
+
+        //public List<VVT_Station> GetAllStations() {
+        public void GetAllStations() {
+
+
+            //Call Api
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://smartinfo.ivb.at/api/JSON");
+            var response = client.GetAsync("/STOPS").Result;
+
+            if (response.IsSuccessStatusCode) {
+                var jsonString = response.Content.ReadAsStringAsync();
+                jsonString.Wait();
+            }
+
+    }
+}
--- HCIN_SWP_Alexa_VVT/Models/VVT_Station.cs
+++ HCIN_SWP_Alexa_VVT/Models/VVT_Station.cs
@@ -1,7 +1,13 @@
-﻿namespace HCIN_SWP_Alexa_VVT.Models
+﻿using Microsoft.Net.Http.Headers;
+
+namespace HCIN_SWP_Alexa_VVT.Models
 {
     public class VVT_Station
     {
+        public int VVTStation { get; set; }
+        public string Name { get; set; }
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
 
     }
 }
